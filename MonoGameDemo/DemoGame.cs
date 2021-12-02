1bbc510cea5dad6e04662156bfc2e33f72e45986
Julian Lawrance <julian.lawrance@gmail.com>
2021-12-02 23:02:20 +0000

    Initial commit

---
+++ MonoGameDemo/DemoGame.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameDemo
+{
+    public class DemoGame : Game
+    {
+        public DemoGame()
+        {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        public GraphicsDeviceManager Graphics { get; }
+        public SpriteBatch SpriteBatch { get; private set; }
+
+        protected override void Initialize()
+        {
+            Components.Add(new Ball(this));
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MonoGameDemo
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new DemoGame();
+
+            game.Run();
+        }
+    }
+}
